57ebfb491c732849c96fe0398c29c8080dc5b709
Pranita-p <pranitappatil2001@gmail.com>
2024-04-15 18:33:03 +0530

    Initial commit

---
+++ GitSample.java
@@ -0,0 +1,13 @@
+public class GitSample{
+
+    public static void main(String[] args){
+        System.out.println("added the item in cart:");
+
+        String items;
+
+        System.out.println("Milk, Bread, Egg, Butter");
+
+
+
+    }
+}
